416db0f0cb54cc7c359b2072a5f7323f4966c674
Yijie Zou <yijiezou2004@gmail.com>
2025-11-13 14:27:06 -0700

    added sidebar links

--- app/components/Sidebar/index.tsx
+++ app/components/Sidebar/index.tsx
@@ -2,16 +2,25 @@
 
 import React, { useState } from 'react'
 import Image from "next/image"
-import { LockIcon } from 'lucide-react';
+import { Briefcase, Home, LockIcon, LucideIcon, Search, Settings, User, Users, X, } from 'lucide-react';
+import { usePathname } from 'next/navigation';
+import { useAppDispatch, useAppSelector } from '@/app/redux';
+import Link from 'next/link';
+import { setIsSidebarCollapsed } from '@/state';
 
-const SideBar = () => {
+const Sidebar = () => {
     const [showProjects, setShowProjects] = useState(true);
     const [showPriority, setShowPriority] = useState(true)
 
+    const dispatch = useAppDispatch();
+
+    const isSidebarCollapsed = useAppSelector(
+        (state) => state.global.isSidebarCollapsed,
+    )
 
     const sidebarClassNames = `fixed flex flex-col h-[100%] justify-between shadow-xl 
     transition-all duration-300 h-full z-40 dark:bg-black overflow-y-auto bg-white
-    w-64
+    ${isSidebarCollapsed ? "w-0 hidden" : "w-64"}}
     `
 
 
@@ -25,8 +34,20 @@ const SideBar = () => {
                     <div className='text-xl font-bold text-gray-800 dark:text-white'>
                         EDLIST
                     </div>
+
+                    {isSidebarCollapsed ?
+                        null :
+                        (<button className="py-3" onClick={() => dispatch(setIsSidebarCollapsed(!isSidebarCollapsed))}>
+
+                            <X className='h-6 w-6 text-gray-800 hover:text-gray-500 dark:text-white' />
+                        
+                        </button>)
+                    }
+
+
+
                 </div>
-    
+
                 {/* Teams */}
 
                 <div className='flex items-center gap-5 border-y-[1.5px] border-gray-200 px-8 py-4 dark:border-gray-700'>
@@ -48,10 +69,81 @@ const SideBar = () => {
 
 
                 {/* Navbar Links */}
-                
+                <nav className="z-10 w-full" >
+                    <SidebarLink icon={Home}
+                        label="Home"
+                        href="/"
+                    />
+                    <SidebarLink icon={Briefcase}
+                        label="Timeline"
+                        href="/timeline"
+                    />
+                    <SidebarLink icon={Search}
+                        label="Search"
+                        href="/search"
+                    />
+                    <SidebarLink icon={Settings}
+                        label="Settings"
+                        href="/settings"
+                    />
+                    <SidebarLink icon={User}
+                        label="Users"
+                        href="/users"
+                    />
+                    <SidebarLink icon={Users}
+                        label="Teams"
+                        href="/teams"
+                    />
+                </nav>
+
+
             </div>
         </div>
     )
 }
 
-export default SideBar
+interface SidebarLinkProps {
+    href: string;
+    icon: LucideIcon;
+    label: string;
+    // isCollapsed: boolean
+}
+
+const SidebarLink = ({
+    href,
+    icon: Icon,
+    label,
+    // isCollapsed
+}:
+    SidebarLinkProps) => {
+    const pathname = usePathname();
+    const isActive = pathname === href || ([pathname === "/" && href === "/dashboard"])
+    // const screenWidth = window.innerWidth;
+
+    // const dispatch = useAppDispatch();
+
+    // const isSidebarCollapsed = useAppSelector(
+    //     (state) => state.global.isSidebarCollapsed,
+    // )
+
+    return (
+        <Link href={href} className="w-full">
+            <div className={`relative flex cursor-pointer items-center gap-3 transition-colors hover:bg-gray-100 dark:bg-black dark:hover:bg-gray-700 
+            ${isActive
+                    ? "bg-gray-100 text-white dark:bg-gray-600" : ""} justify-start px-8 py-3`
+            } >
+                {isActive && (<div className='absolute left-0 top-0 h-[100%] w-[5px] bg-blue-200' />)}
+
+
+                <Icon className='h-6 w-6 text-gray-800 dark:text-gray-100' />
+
+                <span className={`font-medium text-gray-800 dark:text-gray-100`}>
+                    {label}
+                </span>
+            </div>
+        </Link>
+    )
+}
+
+
+export default Sidebar
